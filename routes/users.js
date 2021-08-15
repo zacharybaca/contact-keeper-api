ad5902da4ec6baac97a1d72fcb07b1663f3ad565
zacharybaca <bacazach@icloud.com>
2021-08-14 19:24:13 -0500

    implemented saving users to database with hashed password

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -18,12 +19,39 @@ router.post('/',
         'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
     ],
-     (req, res) => {
+    async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-});
+
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email: email });
+
+        if (user) {
+            return res.status(400).json({ msg: 'User already exists' });
+        }
+
+        user = new User({
+            name: name,
+            email: email,
+            password: password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+    }
+);
 
 
 module.exports = router;

e3cff502ef7dbb20be3c3f10f2e6cc9a6c1cbc95
zacharybaca <bacazach@icloud.com>
2021-08-20 05:10:36 -0500

    implemented update and remove of contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -48,5 +48,58 @@ router.post('/', [auth, [
     }
 });
 
+// @route   PUT api/contacts/:id
+// @desc    Update Contact
+// @access Private
+router.put('/:id', auth, async (req, res) => {
+    const { name, email, phone, type } = req.body;
+
+    const contactFields = {};
+    if (name) contactFields.name = name;
+    if (email) contactFields.email = email;
+    if (phone) contactFields.phone = phone;
+    if (type) contactFields.type = type;
+
+    try {
+        let contact = await Contact.findById(req.params.id);
+
+        if (!contact) return res.status(404).json({ msg: 'Contact Not Found' });
+
+        if (contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not Authorized' });
+        }
+
+        contact = await Contact.findByIdAndUpdate(req.params.id,
+            { $set: contactFields },
+            { new: true }
+            );
+            res.json(contact);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   DELETE api/contacts/:id
+// @desc    Delete Contact
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        let contact = await Contact.findById(req.params.id);
+
+        if (!contact) return res.status(404).json({ msg: 'Contact Not Found' });
+
+        if (contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not Authorized' });
+        }
+
+        await Contact.findByIdAndRemove(req.params.id);
+        res.json({ msg: 'Contact Removed' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 
 module.exports = router;

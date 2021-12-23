cdd505289eb7458282615154d8c8581ffeb7e146
zacharybaca <bacazach@icloud.com>
2021-12-23 07:55:15 -0600

    changed server port to avoid conflicts

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
